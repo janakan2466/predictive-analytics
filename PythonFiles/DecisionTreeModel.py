5c25db464a3aee830c38267cb4da817907c83fcf
Janakan <janakan2466@gmail.com>
2021-12-03 01:32:31 -0500

    updates

---
+++ PythonFiles/DecisionTreeModel.py
@@ -0,0 +1,37 @@
+#Janakan Sivaloganthan 20/11/2021 Machine Learning Assignment
+#Decision Tree Model of the Bank Customer Churn dataset
+
+import numpy as np
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier # Decision Tree Classifier
+from sklearn.model_selection import train_test_split #train_test_split function (split the training set)
+from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+from sklearn.preprocessing import LabelEncoder
+from sklearn import tree
+
+df= pd.read_csv('dataset.csv')
+print(df.head())
+
+target= df['Exited']
+df.drop(['Surname','Exited', 'CustomerId', 'RowNumber'], axis= 'columns', inplace= True)
+print(df.info())
+
+le_Geography= LabelEncoder()
+le_Gender= LabelEncoder()
+
+df['Geography_n']= le_Geography.fit_transform(df['Geography'])
+df['Gender_n']= le_Gender.fit_transform(df['Gender'])
+
+df_n = df.drop(['Geography', 'Gender'], axis='columns') #drops the string variables that are not encoded
+print(df_n.head())
+
+X_train, X_test, Y_train, Y_test = train_test_split(df_n, target, test_size=0.3, random_state=100) #df_n represents inputs while target represents output
+
+model= tree.DecisionTreeClassifier() #Create the classifier tree
+model = model.fit(X_train, Y_train) #train the model
+y_pred = model.predict(X_test)
+metrics.accuracy_score(Y_test, y_pred)
+
+print("\n\nAccuracy= " +str(model.score(df_n, target)*100) +"%") #prints the accuracy of the model
+
+
---
+++ PythonFiles/Statistics.py
@@ -0,0 +1,74 @@
+#Janakan Sivaloganthan 20/11/2021 Machine Learning Assignment
+#Script to display the characteristics of the Bank Customer Churn dataset
+
+#import libraries
+from matplotlib import colors
+import numpy as np
+from numpy.core.fromnumeric import size
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df= pd.read_csv('dataset.csv')
+#print(df.head()) #prints the first 5 rows of df "datafile" 
+
+# A distribution of the total distribution of geographical location
+plt.figure(1)
+df.Geography.value_counts().plot(kind='bar', title= "Total Distribution per Geographic Location", color=['green', 'yellow', 'blue'])
+
+# Representation of the summation of target variable values as a pie chart
+plt.figure(2)
+df.Exited.value_counts().plot(kind='pie', colors=['lightgreen', 'red'], title= "Holistic Analysis of Churned Customers", autopct='%1.1f%%') 
+
+# Representation of the summation of the customer gender values
+plt.figure(3)
+df.Gender.value_counts().plot(kind='bar',title= "Distribution per Gender", color=['blue','pink'])
+
+# A distribution of the customers and their specified age values
+plt.figure(4)
+df["Age"].plot(kind = 'hist', title= "Age Statistic")
+
+# Representation of the taget variable vs the customer geographical location
+plt.figure(5)
+sns.countplot(x= 'Exited', hue= 'Geography', data= df).set_title('Target vs Geography')
+
+# Representation of the taget variable vs if the customer has a credit card
+plt.figure(6)
+sns.countplot(x= 'Exited', hue= 'Geography', data= df).set_title('Target vs Geography')
+
+plt.figure(7)
+sns.countplot(x= 'Exited', hue= 'Geography', data= df).set_title('Target vs Geography')
+
+plt.show()
+
+print("Present features")
+print("-------------------")
+print(df.columns())
+
+print("Present null values")
+print("-------------------")
+print(df.isna().sum())
+
+print("\nSum of target variable")
+print("-------------------")
+print(df.Exited.value_counts())
+
+print("\nPresent the dimensions of the dataset")
+print("-------------------")
+print(df.shape)
+
+# print("\nPresent additional information of the dataset")
+# print("-------------------")
+# print(df.describe())
+
+print("\nPresent additional information such as datatype of dataset")
+print("-------------------")
+print(df.info())
+
+print("\nUnique count allows us ")
+print("-------------------")
+print(df.unique())
+
+print("\nPlot a sample graph to showcase the age distribution")
+print("-------------------\n")
+
